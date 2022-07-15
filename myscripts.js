396cba1610223970d1cc65e6d56700bd5f7323f4
Salvador <csalvador58@gmail.com>
2022-07-14 23:23:57 -0700

    Cleaned function and variable names

--- myscripts.js
+++ myscripts.js
@@ -1,41 +1,41 @@
-let playerWin;
-let computerWin;
+let playerWinCount;
+let computerWinCount;
 
 game();
 
 function game() {
     // Start Rock, Paper, Scissors game. Scores are reset to zero.
-    playerWin = 0;
-    computerWin = 0;
+    playerWinCount = 0;
+    computerWinCount = 0;
 
     for (let i = 0; i < 5; i++) {
     // Play five rounds
-        let playerSelection = prompt("Rock, Paper, or Scissors?");
-        playerPick = playerSelection.toLowerCase();
+        let playerAnswer = prompt(`Round ${i+1} - Rock, Paper, or Scissors?`);
+        playerSelection = playerAnswer.toLowerCase();
 
         // Check for invalid player input
-        while ((playerPick != 'rock') && (playerPick != 'paper') && (playerPick != 'scissors')) {
-            playerSelection = prompt("Pleas enter a valid selection - Rock, Paper, or Scissors?");
-            playerPick = playerSelection.toLowerCase(); 
+        while ((playerSelection != 'rock') && (playerSelection != 'paper') && (playerSelection != 'scissors')) {
+            playerAnswer = prompt("Please enter a valid selection - Rock, Paper, or Scissors?");
+            playerSelection = playerAnswer.toLowerCase(); 
         }
 
-        computerSelection = computerPlay();
+        computerSelection = randomSelect();
         playRound(playerSelection, computerSelection);
 
     }
     
     // Display winner and current scores.
-    if (playerWin > computerWin) {
-        console.log(`You win the game! Overall score - You: ${playerWin}, Computer: ${computerWin}`);
-    } else if (playerWin < computerWin) {
-        console.log(`You lost the game. Overall score - You ${playerWin}, Computer: ${computerWin}`);
+    if (playerWinCount > computerWinCount) {
+        console.log(`You win the game! Overall score - You: ${playerWinCount}, Computer: ${computerWinCount}`);
+    } else if (playerWinCount < computerWinCount) {
+        console.log(`You lost the game. Overall score - You ${playerWinCount}, Computer: ${computerWinCount}`);
     } else {
-        console.log(`Tie game!`);
+        console.log(`Tie game! Overall score - You ${playerWinCount}, Computer: ${computerWinCount}`);
     }
 }
 
 
-function computerPlay() {
+function randomSelect() {
     // Randomly selects rock, paper, or scissor. 
     let playOptions = ['rock', 'paper', 'scissors'];
     return playOptions[Math.floor(Math.random() * 3)];
@@ -43,31 +43,29 @@ function computerPlay() {
 
 function playRound(player, computer) {
     // Compare selections to determine winner.
-    
-
-    if (playerPick === computer) {
-        console.log(`It\'s a tie! Both players selected ${playerPick}.`);
+    if (player === computer) {
+        console.log(`It\'s a tie! Both players selected ${player}.`);
     }
-    else if (playerPick === 'rock' && computer === 'scissors') {
+    else if (player === 'rock' && computer === 'scissors') {
         console.log(`Rock beats scissors, You win!`);
-        playerWin++;
-    } else if (playerPick === 'rock' && computer === 'paper') {
+        playerWinCount++;
+    } else if (player === 'rock' && computer === 'paper') {
         console.log(`Paper beats rock, Computer wins!`);
-        computerWin++;
-    } else if (playerPick === 'paper' && computer === 'rock') {
+        computerWinCount++;
+    } else if (player === 'paper' && computer === 'rock') {
         console.log(`Paper beats rock, You win!`);
-        playerWin++;
-    } else if (playerPick === 'paper' && computer === 'scissors') {
+        playerWinCount++;
+    } else if (player === 'paper' && computer === 'scissors') {
         console.log(`Scissors beats paper, Computer wins!`);
-        computerWin++;
-    } else if (playerPick === 'scissors' && computer === 'rock') {
+        computerWinCount++;
+    } else if (player === 'scissors' && computer === 'rock') {
         console.log(`Rock beats scissors, Computer wins!`);
-        computerWin++;
-    } else if (playerPick === 'scissors' && computer === 'paper') {
+        computerWinCount++;
+    } else if (player === 'scissors' && computer === 'paper') {
         console.log('Scissors beats paper, You win!')
-        playerWin++;
+        playerWinCount++;
     } else {
-        console.log(`${playerPick} is an invalid entry, try again!`);
+        console.log(`System error occurred, please try again!`);
     }
 
 }
